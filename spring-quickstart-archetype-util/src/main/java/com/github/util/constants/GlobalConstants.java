592180bebeaa9bb34bab54511b3da080e22f2741
Leonardo Wiest <leonardowiest@hotmail.com>
2020-03-28 16:37:38 -0300

    Doc: Update

---
+++ spring-quickstart-archetype-util/src/main/java/com/github/util/constants/GlobalConstants.java
@@ -0,0 +1,7 @@
+package com.github.util.constants;
+
+public class GlobalConstants {
+
+	public static final String PKG_BASE = "com.github.leonardowiest";
+	public static final String PKG_REST = PKG_BASE + ".rest";
+}
--- spring-quickstart-archetype-util/src/main/java/com/github/util/constants/RestFullConstants.java
+++
@@ -1,5 +0,0 @@
-package com.github.util.constants;
-
-public enum RestFullConstants {
-
-}
