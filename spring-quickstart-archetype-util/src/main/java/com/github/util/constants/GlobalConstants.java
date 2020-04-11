cf7330ec691af210ac3bc1a58d7f1eb3eecbc641
Leonardo Wiest <leonardowiest@hotmail.com>
2020-04-11 15:18:02 -0300

    Doc: Update

--- spring-quickstart-archetype-util/src/main/java/com/github/util/constants/GlobalConstants.java
+++ spring-quickstart-archetype-util/src/main/java/com/github/util/constants/GlobalConstants.java
@@ -4,4 +4,7 @@ public class GlobalConstants {
 
 	public static final String PKG_BASE = "com.github.leonardowiest";
 	public static final String PKG_REST = PKG_BASE + ".rest";
-}
+
+	public static final String DB_CONFIG_PREFIX = "spring.quickstart.archetype";
+	public static final String DATASOURCE_PROPERFIES = "SpringQuickstartArchetypeDataSourceProperties";
+}/
