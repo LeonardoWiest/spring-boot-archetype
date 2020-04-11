14a31ba4c37be38f5af85a4b5c7d76b54666f2ea
Leonardo Wiest <leonardowiest@hotmail.com>
2020-04-11 14:44:27 -0300

    Doc: Update

--- spring-quickstart-archetype-application/src/main/java/com/github/leonardowiest/config/PropertySources.java
+++ spring-quickstart-archetype-application/src/main/java/com/github/leonardowiest/config/PropertySources.java
@@ -3,7 +3,7 @@ package com.github.leonardowiest.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource(value = "classpath:/database/configuration-db-${spring.profiles.active}.properties")
+@PropertySource(value = "classpath:/db/config/configuration-db-${spring.profiles.active}.properties")
 @PropertySource(value = "classpath:application-${spring.profiles.active}.properties")
 @PropertySource(value = "classpath:application.properties")
 @Configuration
--- spring-quickstart-archetype-application/src/main/java/com/github/leonardowiest/config/database/LiquibaseCustomConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.github.leonardowiest.config.database;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-@Configuration
-public class LiquibaseCustomConfiguration {
-
-}
