3834e44510d299ff2856fbb23378fbc71c5cbb1e
Leonardo Wiest <leonardowiest@hotmail.com>
2020-05-01 13:09:25 -0300

    Doc: Prototipagem

--- spring-quickstart-archetype-util/src/main/java/com/github/util/constants/GlobalConstants.java
+++
@@ -1,7 +0,0 @@
-package com.github.util.constants;
-
-public class GlobalConstants {
-
-	public static final String PKG_BASE = "com.github.leonardowiest";
-	public static final String PKG_REST = PKG_BASE + ".rest";
-}
